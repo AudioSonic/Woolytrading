b13a52bff0c886ebff4fd358c6330e017cec901b
Alexander Stach <stach.alex@yahoo.de>
2026-01-24 17:31:31 +0100

    Initial Commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public int? CategoryId { get; set; }
+        public int? ConditionId { get; set; }
+        public int? RarityId { get; set; }
+        public int? TypeId { get; set; }
+        public Category? Category { get; set; }
+        public Conditions? Condition { get; set; }
+        public Rarities? Rarity { get; set; }
+        public Types? Type { get; set; }
+    }
+}
